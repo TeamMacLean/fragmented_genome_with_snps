f0ce3e09ea8730e17e7e63a3eab38ea6c4403504
pilarcormo@gmail.com <pilarcormo@gmail.com>
2014-10-13 16:23:05 +0100

    New datasets created
    Trying the algorithm

--- create_model_genome.rb
+++ create_model_genome.rb
@@ -4,7 +4,7 @@ require_relative 'lib/write_it'
 require_relative 'lib/reform_ratio'
 
 # make the directory to put data files into
-Dir.mkdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}"))
+Dir.mkdir(File.join(Dir.home, "Desktop/fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}"))
 
 # Create the lists of homozygous and heterozygous SNPs
 fasta_file = "TAIR10_chr4.fasta"
@@ -20,10 +20,10 @@ puts "There are #{ht.length} heterozygous SNPs"
 puts "Is there a SNP at the centre of the distribution? -- #{snp_pos.include?(10000000)}"
 
 arabidopsis_c4 = ModelGenome::fasta_to_char_array(fasta_file)
-
-contig_size = 10000 # 10-20kb
+puts "Fragmenting arabidopsis_c4..."
+contig_size = 10000# 10-20kb
 frags = ModelGenome::get_frags(arabidopsis_c4, contig_size)
-
+puts "Done!"
 puts "Arabidopsis chr4 length: #{arabidopsis_c4.length} bases"
 puts "Fragmented seq   length: #{frags.join.length} = close enough? You decide."
 puts "You have created #{frags.length} fragments of sizes #{contig_size}-#{contig_size*2}"
@@ -34,6 +34,7 @@ pos_on_frags, snp_pos_all = ModelGenome::pos_each_frag(snp_pos, frags)
 fastaformat_array = ModelGenome::fasta_array(frags)
 fastaformat_array_shuf = fastaformat_array.shuffle # shuffle it to show that the order doesn't need to be conserved when working out density later on
 
+puts "Creating your files... We're almost done"
 vcf = ModelGenome::vcf_array(frags, pos_on_frags, snp_pos_all, hm, ht)
 
 WriteIt::write_data("arabidopsis_datasets/#{ARGV[0]}/frags.fasta", fastaformat_array)
